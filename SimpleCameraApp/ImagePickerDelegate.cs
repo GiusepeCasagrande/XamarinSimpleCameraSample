a1b9f9985e72717f87b085fe50c94e45e56ce1aa
Giusepe Casagrande <giusepe@gmail.com>
2014-03-14 11:19:32 -0300

    Now Saving the images.

--- SimpleCameraApp/ImagePickerDelegate.cs
+++ SimpleCameraApp/ImagePickerDelegate.cs
@@ -13,6 +13,17 @@ namespace SimpleCameraApp
 		public override void FinishedPickingImage(UIImagePickerController picker, UIImage image, NSDictionary editingInfo)
 		{
 			// Here you can do anything with the image... save for example.
+			var documentsDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+			string jpgFilename = System.IO.Path.Combine(documentsDirectory, string.Format("{0}.jpg", Guid.NewGuid()));
+
+			using (NSData imageData = image.AsJPEG(0.2f))
+			{
+				NSError err;
+				if (!imageData.Save(jpgFilename, false, out err))
+				{
+					Console.WriteLine("Saving of file failed: " + err.Description);
+				}
+			}
 		}
 	}
 }
